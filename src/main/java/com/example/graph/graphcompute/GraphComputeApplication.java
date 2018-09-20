5ed1e4bfe3afe1e2f698398c0719a1277f8f76b9
dipsy88 <dipsy.urfriend@gmail.com>
2018-09-20 13:50:01 +0200

    spring boot project started

---
+++ src/main/java/com/example/graph/graphcompute/GraphComputeApplication.java
@@ -0,0 +1,12 @@
+package com.example.graph.graphcompute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphComputeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphComputeApplication.class, args);
+	}
+}
